820a5defbe6e0c8ca9fee3311bedeefaa1bad353
Upadhyay <aashish.upadhyay@rsa.com>
2020-03-03 18:49:46 -0500

    huffman encoding

---
+++ UnitTestProject/HuffmanTreeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codesthenics;
+using System.Collections.Generic;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class HuffmanTreeTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var huffmanTree = new HuffmanEncoding();
+            var characterFrequencies = new Dictionary<char, int>();
+            characterFrequencies.Add('C', 1);
+            characterFrequencies.Add('A', 1);
+            characterFrequencies.Add('T', 1);
+            characterFrequencies.Add('S', 1);
+            var encoding = huffmanTree.EncodeCharacters(characterFrequencies);
+            Assert.IsTrue(encoding.Count == 4 && encoding['C'] == "00" && encoding['S'] == "01" && encoding['T'] == "10" && encoding['A'] == "11");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var huffmanTree = new HuffmanEncoding();
+            var characterFrequencies = new Dictionary<char, int>();
+            characterFrequencies.Add('a', 3);
+            characterFrequencies.Add('c', 6);
+            characterFrequencies.Add('e', 8);
+            characterFrequencies.Add('f', 2);
+            var encoding = huffmanTree.EncodeCharacters(characterFrequencies);
+            Assert.IsTrue(encoding.Count == 4 && encoding['e'] == "0" && encoding['f'] == "100" && encoding['a'] == "101" && encoding['c'] == "11");
+        }
+    }
+}
