cc1825e2bb987ec24ef687c7acb642e5288c8291
Upadhyay <aashish.upadhyay@rsa.com>
2020-03-17 23:27:37 -0400

    RemoveEdgesToCreateEvenTrees

---
+++ Codesthenics/Graph/RemoveEdgesToCreateEvenTrees.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codesthenics
+{
+    public class RemoveEdgesToCreateEvenTrees
+    {
+        private GraphAJ<int> _graph;
+        public IList<Tuple<int, int>> RemoveableEdges(GraphAJ<int> graph, int root)
+        {
+            _graph = graph;
+            var returnValue = new List<Tuple<int, int>>();
+            var removeableEdges = new Dictionary<string, Edge>();
+
+            var neighbours = _graph.AdjacencyList[root];
+
+            foreach (var neighbor in neighbours)
+            {
+                var descendantCount = CountDescendants(root, neighbor, removeableEdges);
+                if (descendantCount % 2 == 0)
+                {
+                    var edge = new Edge(root, neighbor);
+                    removeableEdges.Add(edge.HashCode, edge);
+                }
+            }
+
+            if (removeableEdges.Count > 0)
+                returnValue = removeableEdges.Values.Select(u => new Tuple<int, int>(u.Vertex1, u.Vertex2)).ToList();
+            return returnValue;
+        }
+
+        private int CountDescendants(int fromVertex, int toVertex, Dictionary<string, Edge> removeableEdges)
+        {
+            var descendantCount = 1;
+            var neighbours = _graph.AdjacencyList[toVertex];
+
+            foreach (var neighbor in neighbours)
+            {
+                var currentDescendantCount = CountDescendants(toVertex, neighbor, removeableEdges);
+                if (currentDescendantCount % 2 == 0)
+                {
+                    var edge = new Edge(toVertex, neighbor);
+                    removeableEdges.Add(edge.HashCode, edge);
+                }
+                descendantCount += currentDescendantCount;
+            }
+
+            return descendantCount;
+        }
+
+        struct Edge
+        {
+            public Edge(int vertex1, int vertex2)
+            {
+                Vertex1 = vertex1;
+                Vertex2 = vertex2;
+            }
+
+            public int Vertex1;
+            public int Vertex2;
+
+            public string HashCode => Vertex1.ToString() + Vertex2.ToString();
+        }
+    }
+}
---
+++ UnitTestProject/RemoveEdgesToCreateEvenTreesTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codesthenics;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class RemoveEdgesToCreateEvenTreesTests
+    {
+        [TestMethod]
+        public void RemoveEdgesToCreateEvenTreesTest1()
+        {
+            var graph = new GraphAJ<int>(
+                new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 },
+                new List<Tuple<int, int>>()
+                {
+                    new Tuple<int, int>(1,2),
+                    new Tuple<int, int>(1,3),
+                    new Tuple<int, int>(3,4),
+                    new Tuple<int, int>(3,5),
+                    new Tuple<int, int>(4,6),
+                    new Tuple<int, int>(4,7),
+                    new Tuple<int, int>(4,8)
+                });
+
+            var removeEdgesToCreateEvenTrees = new RemoveEdgesToCreateEvenTrees();
+            var expected = removeEdgesToCreateEvenTrees.RemoveableEdges(graph, 1);
+
+            Assert.IsTrue(expected.Count == 2);
+        }
+    }
+}
