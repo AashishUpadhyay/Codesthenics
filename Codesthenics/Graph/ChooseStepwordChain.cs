b047c0c23ea3d7b0686b5de55e7b70de50fc7d0b
Upadhyay <aashish.upadhyay@rsa.com>
2020-03-23 23:38:56 -0400

    ChooseStepwordChain

---
+++ Codesthenics/Graph/ChooseStepwordChain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codesthenics
+{
+    public class ChooseStepwordChain
+    {
+        public IList<string> BuildStepwordChain(string startWord, string endWord, HashSet<string> wordListHash)
+        {
+            var transformationSequence = new List<string>() { startWord };
+            var stopProcessing = false;
+
+            while (!stopProcessing)
+            {
+                var transformedWord = TransformWord(transformationSequence[transformationSequence.Count - 1], endWord, wordListHash);
+
+                if (transformedWord.Equals(endWord))
+                {
+                    stopProcessing = true;
+                    transformationSequence.Add(transformedWord);
+                }
+                else if (transformedWord.Equals(string.Empty))
+                {
+                    stopProcessing = true;
+                }
+                else
+                {
+                    transformationSequence.Add(transformedWord);
+                }
+            }
+
+            if (transformationSequence[transformationSequence.Count - 1] == endWord)
+                return transformationSequence;
+
+            return null;
+        }
+
+        private string TransformWord(string wordToTransform, string endWord, HashSet<string> wordListHash)
+        {
+            var wordToTransformAsArray = wordToTransform.ToArray();
+            for (int i = 0; i < wordToTransform.Length; i++)
+            {
+                wordToTransformAsArray[i] = endWord[i];
+                var nw =new string(wordToTransformAsArray);
+                if (wordListHash.Contains(nw))
+                    return nw;
+                else
+                    wordToTransformAsArray[i] = wordToTransform[i];
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ UnitTestProject/ChooseStepwordChainTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codesthenics;
+using System.Collections.Generic;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ChooseStepwordChainTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var chooseStepwordChain = new ChooseStepwordChain();
+            var returnedValue = chooseStepwordChain.BuildStepwordChain("dog", "cat", new System.Collections.Generic.HashSet<string>() {
+                "dot","dop","dat","cat"
+            });
+
+            var expectedValue = new List<string>() { "dog", "dot", "dat", "cat" };
+
+            Assert.IsTrue(returnedValue[0].Equals(expectedValue[0]));
+            Assert.IsTrue(returnedValue[1].Equals(expectedValue[1]));
+            Assert.IsTrue(returnedValue[2].Equals(expectedValue[2]));
+            Assert.IsTrue(returnedValue[3].Equals(expectedValue[3]));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var chooseStepwordChain = new ChooseStepwordChain();
+            var returnedValue = chooseStepwordChain.BuildStepwordChain("dog", "cat", new System.Collections.Generic.HashSet<string>() {
+                "dop","dat","cat"
+            });
+
+
+            Assert.IsTrue(returnedValue == null);
+        }
+    }
+}
