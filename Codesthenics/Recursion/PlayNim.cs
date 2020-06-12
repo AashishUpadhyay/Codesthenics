9459c939925583fb4d62b8fd8d09d0903c933b30
Upadhyay <aashish.upadhyay@rsa.com>
2020-06-12 18:30:43 -0400

    PlayNim

---
+++ Codesthenics/Recursion/PlayNim.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codesthenics
+{
+	public class PlayNim
+	{
+		public bool CanWin(int[] nums)
+		{
+			if (nums[0] == 0 && nums[1] == 0 && nums[2] == 0)
+				return true;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				for (int j = 1; j < nums[i]; j++)
+				{
+					var preList = nums.Take(i).ToList();
+					preList.Add(nums[i] - j);
+					preList.AddRange(nums.Skip(i + 1).Take(nums.Length - (i + 1)));
+					if (!CanWin(preList.ToArray()))
+						return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ UnitTestProject/PlayNimTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codesthenics;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class PlayNimTests
+	{
+		[TestMethod]
+		public void PlayNimTest()
+		{
+			var playNim = new PlayNim();
+			Assert.IsTrue(playNim.CanWin(new int[] { 2, 1, 1 }));
+			Assert.IsTrue(playNim.CanWin(new int[] { 1, 1, 2 }));
+			Assert.IsTrue(playNim.CanWin(new int[] { 1, 2, 1 }));
+			Assert.IsFalse(playNim.CanWin(new int[] { 1, 1, 1 }));
+			Assert.IsFalse(playNim.CanWin(new int[] { 0, 0, 1 }));
+			Assert.IsFalse(playNim.CanWin(new int[] { 0, 1, 0 }));
+			Assert.IsFalse(playNim.CanWin(new int[] { 1, 0, 0 }));
+			Assert.IsTrue(playNim.CanWin(new int[] { 3, 4, 5 }));
+			Assert.IsTrue(playNim.CanWin(new int[] { 1, 2, 3 }));
+
+
+			Assert.IsTrue(playNim.CanWin(new int[] { 3, 3, 3 }));
+			Assert.IsFalse(playNim.CanWin(new int[] { 1, 3, 3 }));
+		}
+	}
+}
