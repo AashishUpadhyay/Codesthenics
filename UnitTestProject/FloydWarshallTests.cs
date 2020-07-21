4143641ea2871f101bd9d91156aa8ea17522c1c3
Upadhyay <aashish.upadhyay@rsa.com>
2020-07-21 01:15:10 -0400

    FloydWarshall

---
+++ UnitTestProject/FloydWarshallTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codesthenics;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class FloydWarshallTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			var fw = new FloydWarshall();
+			var returedVal = fw.BuildGraph(new int[2][] {
+			new int[3]{0,1,2},
+			new int[3]{1,2,3},
+			}, 3);
+			Assert.IsTrue(returedVal[0][1] == 2);
+			Assert.IsTrue(returedVal[0][2] == 5);
+			Assert.IsTrue(returedVal[1][2] == 3);
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			var fw = new FloydWarshall();
+			var returedVal = fw.BuildGraph(new int[6][] {
+			new int[3]{0,1,5},
+			new int[3]{0,5,4},
+			new int[3]{0,2,3},
+			new int[3]{2,3,1},
+			new int[3]{3,4,5},
+			new int[3]{3,5,10}
+			}, 6);
+			Assert.IsTrue(returedVal[0][3] == 4);
+			Assert.IsTrue(returedVal[0][4] == 9);
+			Assert.IsTrue(returedVal[0][5] == 4);
+		}
+	}
+}
---
+++ src/Algorithms/FloydWarshall.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codesthenics
+{
+	public class FloydWarshall
+	{
+		private const int CURRENT_CONTEXT_INFINITI = 10000000;
+		int m_vertices;
+		private int[][] graph;
+		public int[][] BuildGraph(int[][] input, int vertices)
+		{
+			m_vertices = vertices;
+			graph = new int[vertices][];
+			for (int m = 0; m < vertices; m++)
+			{
+				graph[m] = new int[vertices];
+				for (int n = 0; n < vertices; n++)
+				{
+					graph[m][n] = CURRENT_CONTEXT_INFINITI;
+				}
+			}
+
+			BuildGraphInternal(input);
+
+			for (int k = 0; k < vertices; k++)
+			{
+				for (int i = 0; i < vertices; i++)
+				{
+					for (int j = 0; j < vertices; j++)
+					{
+						if (graph[i][j] > (graph[i][k] + graph[k][j]))
+							graph[i][j] = graph[i][k] + graph[k][j];
+					}
+				}
+			}
+
+			return graph;
+		}
+
+		private void BuildGraphInternal(int[][] input)
+		{
+			foreach (var item in input)
+			{
+				for (int i = 0; i < item.Length; i++)
+				{
+					if (graph[item[0]] == null)
+						graph[item[0]] = new int[m_vertices];
+
+					graph[item[0]][item[1]] = item[2];
+				}
+			}
+		}
+	}
+}
