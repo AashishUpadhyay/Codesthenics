a82496608ef6e03818ef56d08743c4b7ef9fc259
Upadhyay <aashish.upadhyay@rsa.com>
2020-08-04 16:24:31 -0400

    fenwick tree revision

--- src/AdvancedDataStructures/FenwickTree.cs
+++ src/AdvancedDataStructures/FenwickTree.cs
@@ -6,40 +6,38 @@ using System.Threading.Tasks;
 
 namespace Codesthenics
 {
-    /// <summary>
-    /// Reference 
-    /// https://notes.tweakblogs.net/blog/9835/fenwick-trees-demystified.html
-    /// </summary>
-    public class FenwickTree
-    {
-        public int[] Create(int[] input)
-        {
-            var returnValue = new int[input.Length];
-            for (int i = 0; i < input.Length; i++)
-            {
-                Update(i, input[i], returnValue);
-            }
-            return returnValue;
-        }
+	/// <summary>
+	/// Reference 
+	/// https://notes.tweakblogs.net/blog/9835/fenwick-trees-demystified.html
+	/// </summary>
+	public class FenwickTree
+	{
+		public int[] Create(int[] input)
+		{
+			var returnValue = new int[input.Length];
+			for (int i = 0; i < input.Length; i++)
+				Update(i, input[i], returnValue);
+			return returnValue;
+		}
 
-        public void Update(int index, int value, int[] target)
-        {
-            while (index < target.Length)
-            {
-                target[index] += value;
-                index |= index + 1;
-            }
-        }
+		public void Update(int index, int value, int[] target)
+		{
+			while (index < target.Length)
+			{
+				target[index] += value;
+				index |= (index + 1);
+			}
+		}
 
-        public int Query(int count, int[] target)
-        {
-            var returnValue = 0;
-            while (count > 0)
-            {
-                returnValue += target[count - 1];
-                count &= count - 1;
-            }
-            return returnValue;
-        }
-    }
+		public int Query(int count, int[] target)
+		{
+			int returnValue = 0;
+			while (count > 0)
+			{
+				returnValue += target[count-1];
+				count &= (count - 1);
+			}
+			return returnValue;
+		}
+	}
 }
