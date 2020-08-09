8c80a3acfbdf780319896d2cbcf21ee0b2104569
Upadhyay <aashish.upadhyay@rsa.com>
2020-08-09 11:00:04 -0400

    BrokenKeyboard.cs

---
+++ UnitTestProject/BrokenKeyboardTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codesthenics;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class BrokenKeyboardTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			var bk = new BrokenKeyboard();
+			var retVal = bk.ActualInputs("can s r n ", new List<string>() { "caneserene", "caneseren", "can", "canes", "serene", "rene", "sam", "s", "r", "n", "n " });
+			var retValHS = retVal.ToHashSet();
+
+			Assert.IsTrue(retValHS.Contains("caneserene"));
+			Assert.IsTrue(retValHS.Contains("caneseren"));
+			Assert.IsTrue(retValHS.Contains("can serene"));
+			Assert.IsTrue(retValHS.Contains("canes rene"));
+			Assert.IsTrue(retValHS.Contains("can s r n"));
+			Assert.IsTrue(retValHS.Contains("can s r n "));
+		}
+	}
+}
