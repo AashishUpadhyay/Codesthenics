876019f509992017ded7da978e931e09ae3b4617
Upadhyay <aashish.upadhyay@rsa.com>
2020-09-13 15:12:42 -0400

    NumberOfWaysToDecodeStringTests

--- UnitTestProject/NumberOfWaysToDecodeStringTests.cs
+++ UnitTestProject/NumberOfWaysToDecodeStringTests.cs
@@ -16,7 +16,8 @@ namespace UnitTestProject
 		{
 			var nwtds = new NumberOfWaysToDecodeString();
 			var ways = nwtds.NumberOfWays("111");
-			Assert.IsTrue(ways.Count == 3);
+			var w = nwtds.Count("111");
+			Assert.IsTrue(ways.Count == w);
 		}
 
 		[TestMethod]
@@ -24,7 +25,8 @@ namespace UnitTestProject
 		{
 			var nwtds = new NumberOfWaysToDecodeString();
 			var ways = nwtds.NumberOfWays("11111");
-			Assert.IsTrue(ways.Count == 8);
+			var w = nwtds.Count("11111");
+			Assert.IsTrue(ways.Count == w);
 		}
 
 		[TestMethod]
@@ -32,7 +34,8 @@ namespace UnitTestProject
 		{
 			var nwtds = new NumberOfWaysToDecodeString();
 			var ways = nwtds.NumberOfWays("133");
-			Assert.IsTrue(ways.Count == 2);
+			var w = nwtds.Count("133");
+			Assert.IsTrue(ways.Count == w);
 		}
 
 		[TestMethod]
@@ -40,16 +43,35 @@ namespace UnitTestProject
 		{
 			var nwtds = new NumberOfWaysToDecodeString();
 			var ways = nwtds.NumberOfWays("3133");
-			Assert.IsTrue(ways.Count == 2);
+			var w = nwtds.Count("3133");
+			Assert.IsTrue(ways.Count == w);
 		}
-
 
 		[TestMethod]
 		public void Test5()
 		{
 			var nwtds = new NumberOfWaysToDecodeString();
 			var ways = nwtds.NumberOfWays("1133");
-			Assert.IsTrue(ways.Count == 3);
+			var w = nwtds.Count("1133");
+			Assert.IsTrue(ways.Count == w);
+		}
+
+		[TestMethod]
+		public void Test6()
+		{
+			var nwtds = new NumberOfWaysToDecodeString();
+			var ways = nwtds.NumberOfWays("993");
+			var w = nwtds.Count("993");
+			Assert.IsTrue(ways.Count == w);
+		}
+
+		[TestMethod]
+		public void Test7()
+		{
+			var nwtds = new NumberOfWaysToDecodeString();
+			var ways = nwtds.NumberOfWays("141131331111111339936341111114116343133111133111");
+			var w = nwtds.Count("141131331111111339936341111114116343133111133111");
+			Assert.IsTrue(ways.Count == w);
 		}
 	}
 }
--- src/DynamicProgramming/NumberOfWaysToDecodeString.cs
+++ src/DynamicProgramming/NumberOfWaysToDecodeString.cs
@@ -56,5 +56,50 @@ namespace Codesthenics
 			dict.Add(s, decodedString.ToList());
 			return decodedString.ToList();
 		}
+
+		private Dictionary<string, int> codesDict = new Dictionary<string, int>() {
+		 {"1", 1 },
+		 {"2", 1 },
+		 {"3", 1 },
+		 {"4", 1 },
+		 {"5", 1 },
+		 {"6", 1 },
+		 {"7", 1 },
+		 {"8", 1 },
+		 {"9", 1 },
+		 {"10", 1 },
+		 {"11", 2 },
+		 {"12", 2 },
+		 {"13", 2 },
+		 {"14", 2 },
+		 {"15", 2 },
+		 {"16", 2 },
+		 {"17", 2 },
+		 {"18", 2 },
+		 {"19", 2 },
+		 {"20", 1 },
+		 {"21", 2 },
+		 {"22", 2 },
+		 {"23", 2 },
+		 {"24", 2 },
+		 {"25", 2 },
+		 {"26", 2 }
+		};
+
+		public int Count(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+				return 0;
+
+			if (codesDict.ContainsKey(s))
+				return codesDict[s];
+
+			int total = Count(s.Substring(1, s.Length - 1));
+			if (codesDict.ContainsKey(s.Substring(0, 2)))
+				total += Count(s.Substring(2, s.Length - 2));
+
+			codesDict.Add(s, total);
+			return codesDict[s];
+		}
 	}
 }
