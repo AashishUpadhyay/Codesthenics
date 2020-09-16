69578175dea36599edafe237286a4e387fc692bf
Upadhyay <aashish.upadhyay@rsa.com>
2020-09-15 22:09:11 -0400

    code optimzation

--- UnitTestProject/MinimumCostToPaintHousesTests.cs
+++ UnitTestProject/MinimumCostToPaintHousesTests.cs
@@ -21,7 +21,13 @@ namespace UnitTestProject
 				new int[3]{4, 18, 4},
 				new int[3]{9, 14, 6},
 			});
-			Assert.IsTrue(cost == 15);
+			var dpcost = mctph.CostBruteDP(new int[3][]
+			{
+				new int[3]{12, 16, 5},
+				new int[3]{4, 18, 4},
+				new int[3]{9, 14, 6},
+			});
+			Assert.IsTrue(cost == dpcost);
 		}
 
 		[TestMethod]
@@ -34,7 +40,13 @@ namespace UnitTestProject
 				new int[3]{16,16,5},
 				new int[3]{14,3,19},
 			});
-			Assert.IsTrue(cost == 10);
+			var dpcost = mctph.CostBruteDP(new int[3][]
+			{
+				new int[3]{17,2,17},
+				new int[3]{16,16,5},
+				new int[3]{14,3,19},
+			});
+			Assert.IsTrue(cost == dpcost);
 		}
 
 		[TestMethod]
@@ -42,7 +54,8 @@ namespace UnitTestProject
 		{
 			var mctph = new MinimumCostToPaintHouses();
 			var cost = mctph.CostBruteForce(new int[0][]);
-			Assert.IsTrue(cost == 0);
+			var dpcost = mctph.CostBruteDP(new int[0][]);
+			Assert.IsTrue(cost == dpcost);
 		}
 
 		[TestMethod]
@@ -68,7 +81,26 @@ namespace UnitTestProject
 			new int[3]{14,6,13},
 			new int[3]{15,7,6}
 			});
-			Assert.IsTrue(cost == 106);
+			var dpcost = mctph.CostBruteDP(new int[17][] {
+			new int[3]{7,8,12},
+			new int[3]{18,16,1},
+			new int[3]{14,7,7},
+			new int[3]{16,2,20},
+			new int[3]{9,13,18},
+			new int[3]{14,18,17},
+			new int[3]{7,10,6},
+			new int[3]{8,4,11},
+			new int[3]{20,5,2},
+			new int[3]{9,8,4},
+			new int[3]{13,1,6},
+			new int[3]{2,3,10},
+			new int[3]{7,4,16},
+			new int[3]{7,19,17},
+			new int[3]{10,6,8},
+			new int[3]{14,6,13},
+			new int[3]{15,7,6}
+			});
+			Assert.IsTrue(cost == dpcost);
 		}
 
 		[TestMethod]
@@ -114,7 +146,53 @@ namespace UnitTestProject
 			new int[3]{6,19,16},
 			new int[3]{20,1,10}
 			});
-			Assert.IsTrue(cost == 268);
+			Assert.IsTrue(268 == cost);
+		}
+
+		[TestMethod]
+		public void Test6()
+		{
+			var mctph = new MinimumCostToPaintHouses();
+			var dpcost = mctph.CostBruteDP(new int[37][] {
+			new int[3]{10,6,7},
+			new int[3]{19,9,8},
+			new int[3]{18,20,3},
+			new int[3]{17,8,13},
+			new int[3]{15,11,16},
+			new int[3]{11,20,10},
+			new int[3]{8,6,5},
+			new int[3]{5,19,5},
+			new int[3]{14,14,20},
+			new int[3]{6,6,1},
+			new int[3]{15,3,12},
+			new int[3]{17,7,5},
+			new int[3]{7,6,8},
+			new int[3]{19,5,6},
+			new int[3]{15,10,7},
+			new int[3]{19,4,12},
+			new int[3]{13,8,16},
+			new int[3]{3,14,12},
+			new int[3]{4,12,5},
+			new int[3]{19,20,3},
+			new int[3]{19,10,15},
+			new int[3]{1,7,17},
+			new int[3]{6,15,13},
+			new int[3]{11,6,20},
+			new int[3]{7,6,7},
+			new int[3]{14,13,15},
+			new int[3]{19,17,13},
+			new int[3]{5,11,8},
+			new int[3]{2,17,12},
+			new int[3]{12,13,4},
+			new int[3]{9,19,4},
+			new int[3]{20,5,6},
+			new int[3]{20,16,7},
+			new int[3]{17,18,3},
+			new int[3]{8,10,2},
+			new int[3]{6,19,16},
+			new int[3]{20,1,10}
+			});
+			Assert.IsTrue(dpcost == 268);
 		}
 	}
 }
--- src/DynamicProgramming/MinimumCostToPaintHouses.cs
+++ src/DynamicProgramming/MinimumCostToPaintHouses.cs
@@ -38,5 +38,49 @@ namespace Codesthenics
 				CostBruteForcePrivate(ri + 1, i, costSoFar + costColorArr[ri][i]);
 			}
 		}
+
+		private int[][] _dp;
+		private int dpMinCost = Int32.MaxValue;
+		public int CostBruteDP(int[][] arr)
+		{
+			if (arr.Length == 0)
+				return 0;
+
+			costColorArr = arr;
+			_dp = new int[arr.Length][];
+			for (int i = 0; i < arr.Length; i++)
+				_dp[i] = new int[arr[0].Length];
+
+			for (int i = 0; i < arr[0].Length; i++)
+			{
+				int cost = CostBruteDPPrivate(0, i);
+				if (cost < dpMinCost)
+					dpMinCost = cost;
+			}
+
+			return dpMinCost == Int32.MaxValue ? 0 : dpMinCost;
+		}
+
+		private int CostBruteDPPrivate(int ri, int ci)
+		{
+			if (ri > costColorArr.Length - 1)
+				return 0;
+
+			if (_dp[ri][ci] != 0)
+				return _dp[ri][ci];
+
+			int cost = Int32.MaxValue;
+			for (int i = 0; i < costColorArr[0].Length; i++)
+			{
+				if (i == ci)
+					continue;
+
+				int bottomUpCost = CostBruteDPPrivate(ri + 1, i);
+				if (bottomUpCost < cost)
+					cost = bottomUpCost;
+			}
+			_dp[ri][ci] = cost + costColorArr[ri][ci];
+			return _dp[ri][ci];
+		}
 	}
 }
