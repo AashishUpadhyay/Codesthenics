2e6bac91d02f5945c29d2af6b617d0868b2fce8a
Upadhyay <aashish.upadhyay@rsa.com>
2020-11-16 13:15:55 -0500

    MaximumRectangle

---
+++ UnitTestProject/MaximumRectangleTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codesthenics;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class MaximumRectangleTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			var grid = new int[3][]
+				{
+					new int[3]{1,1,0},
+					new int[3]{1,1,0},
+					new int[3]{1,1,0},
+				};
+
+			var ar = (new MaximumRectangle()).Area(grid);
+			Assert.IsTrue(ar == 6);
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			var grid = new int[4][]
+				{
+					new int[5]{1,0,1,0,0},
+					new int[5]{1,0,1,1,1},
+					new int[5]{1,1,1,1,1},
+					new int[5]{1,0,0,1,0},
+				};
+
+			var ar = (new MaximumRectangle()).Area(grid);
+			Assert.IsTrue(ar == 6);
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			var grid = new int[0][];
+
+			var ar = (new MaximumRectangle()).Area(grid);
+			Assert.IsTrue(ar == 0);
+		}
+
+		[TestMethod]
+		public void Test4()
+		{
+			var grid = new int[1][] {
+				new int[1]{0}
+			};
+
+			var ar = (new MaximumRectangle()).Area(grid);
+			Assert.IsTrue(ar == 0);
+		}
+
+		[TestMethod]
+		public void Test5()
+		{
+			var grid = new int[1][] {
+				new int[1]{1}
+			};
+
+			var ar = (new MaximumRectangle()).Area(grid);
+			Assert.IsTrue(ar == 1);
+		}
+
+		[TestMethod]
+		public void Test6()
+		{
+			var grid = new int[1][] {
+				new int[2]{0,0}
+			};
+
+			var ar = (new MaximumRectangle()).Area(grid);
+			Assert.IsTrue(ar == 0);
+		}
+	}
+}
---
+++ src/Matrix/MaximumRectangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codesthenics
+{
+	public class MaximumRectangle
+	{
+		private int _len = 0;
+		private int _wid = 0;
+		private int _area = 0;
+		private int[][] _matrix;
+		public int Area(int[][] matrix)
+		{
+			_matrix = matrix;
+
+			if (_matrix.Length == 0)
+				return 0;
+
+			_len = _matrix.Length;
+			if (_matrix[0].Length > 0)
+				_wid = _matrix[0].Length;
+
+			for (int i = 0; i < _len; i++)
+			{
+				for (int j = 0; j < _wid; j++)
+				{
+					int[] _visited = new int[_wid];
+					CalculateArea(i, j, 1, _visited, 0);
+				}
+			}
+
+			return _area;
+		}
+
+		private void CalculateArea(int i, int j, int width, int[] visited, int height)
+		{
+			if (j < 0 || j > _wid - 1 || _matrix[i][j] == 0 || visited[j] == 1)
+				return;
+
+			visited[j] = 1;
+
+			int ht = -1;
+			if (height == 0)
+				ht = CalculateHeight(i, j);
+			else
+				ht = Math.Min(height, CalculateHeight(i, j));
+
+			_area = Math.Max(_area, ht * width);
+
+			CalculateArea(i, j + 1, width + 1, visited, ht);
+			CalculateArea(i, j - 1, width + 1, visited, ht);
+		}
+
+		private int CalculateHeight(int i, int j)
+		{
+			int ht = 0;
+			for (int m = i; m > -1; m--)
+			{
+				if (_matrix[m][j] == 1)
+					ht++;
+			}
+			return ht;
+		}
+	}
+
+}
