7802f79e01c59f8b379064d94f4be78d3120a619
Upadhyay <aashish.upadhyay@rsa.com>
2021-02-15 16:54:19 -0500

    LruCache

---
+++ UnitTestProject/FindTwoNonOverlappingSubArraysEachWithTargetSumTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codesthenics;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class FindTwoNonOverlappingSubArraysEachWithTargetSumTests
+	{
+		[TestMethod]
+		public void Test4()
+		{
+			var ftno = new FindTwoNonOverlappingSubArraysEachWithTargetSum();
+
+			var retVal = ftno.MinSumOfLengths(new int[] { 3,2,2,4,3 }, 3);
+			Assert.IsTrue(retVal == 2);
+		}
+
+		[TestMethod]
+		public void Test1()
+		{
+			var ftno = new FindTwoNonOverlappingSubArraysEachWithTargetSum();
+
+			var retVal = ftno.MinSumOfLengths(new int[] { 1, 2, 2, 3, 2, 6, 7, 2, 1, 4, 8 }, 5);
+			Assert.IsTrue(retVal == 4);
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			var ftno = new FindTwoNonOverlappingSubArraysEachWithTargetSum();
+
+			var retVal = ftno.MinSumOfLengths(new int[] { 1, 6, 1 }, 7);
+			Assert.IsTrue(retVal == -1);
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			var ftno = new FindTwoNonOverlappingSubArraysEachWithTargetSum();
+
+			var retVal = ftno.MinSumOfLengths(new int[] { 1, 1, 1, 2, 2, 2, 4, 4 }, 6);
+			Assert.IsTrue(retVal == 6);
+		}
+
+		[TestMethod]
+		public void Test5()
+		{
+			var ftno = new FindTwoNonOverlappingSubArraysEachWithTargetSum();
+
+			var retVal = ftno.MinSumOfLengths(new int[] { 2, 2, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 20);
+			Assert.IsTrue(retVal == 23);
+		}
+	}
+}
+
---
+++ src/LruCache/LruCacheRevision.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codesthenics
+{
+	public class LruCacheRevision
+	{
+		private readonly Dictionary<int, LinkedListNode<int[]>> _cache;
+		private readonly LinkedList<int[]> _linkedList;
+		private readonly int _size;
+
+		public LruCacheRevision(int size = 5)
+		{
+			_size = size;
+			_cache = new Dictionary<int, LinkedListNode<int[]>>();
+			_linkedList = new LinkedList<int[]>();
+		}
+
+		public void Put(int key, int value)
+		{
+			if (_cache.ContainsKey(key))
+			{
+				_cache[key].Value[1] = value;
+				_linkedList.Remove(_cache[key]);
+				_linkedList.AddFirst(_cache[key]);
+				return;
+			}
+
+			if (_cache.Count() < _size)
+			{
+				_cache.Add(key, new LinkedListNode<int[]>(new int[2] { key, value }));
+				_linkedList.AddFirst(_cache[key]);
+				return;
+			}
+
+			var itemToRemove = _linkedList.Last();
+			_linkedList.Remove(itemToRemove);
+			_cache.Remove(itemToRemove[0]);
+
+			_cache.Add(key, new LinkedListNode<int[]>(new int[2] { key, value }));
+			_linkedList.AddFirst(_cache[key]);
+
+			return;
+		}
+
+		public int Get(int key)
+		{
+			if (!_cache.ContainsKey(key))
+				return -1;
+
+			_linkedList.Remove(_cache[key]);
+			_linkedList.AddFirst(_cache[key]);
+
+			return _cache[key].Value[1];
+		}
+	}
+}
