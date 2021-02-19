ea5f5d0d6019402c30e01788f414ac387541bd86
Upadhyay <aashish.upadhyay@rsa.com>
2021-02-19 13:52:55 -0500

    binary search revision

---
+++ UnitTestProject/BinarySearchTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codesthenics;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class BinarySearchTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			var bs = new BinarySearch();
+			var result = bs.Find(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 1);
+			Assert.IsTrue(result == 0);
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			var bs = new BinarySearch();
+			var result = bs.Find(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 10);
+			Assert.IsTrue(result == 9);
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			var bs = new BinarySearch();
+			var result = bs.Find(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 7);
+			Assert.IsTrue(result == 6);
+		}
+
+		[TestMethod]
+		public void Test4()
+		{
+			var bs = new BinarySearch();
+			var result = bs.Find(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 7, 7, 10 }, 7);
+			Assert.IsTrue(result == 6 || result == 7 || result == 8);
+		}
+
+		[TestMethod]
+		public void Test5()
+		{
+			var bs = new BinarySearch();
+			var result = bs.FindLeftMost(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 7, 7, 10 }, 1);
+			Assert.IsTrue(result == 0);
+		}
+
+		[TestMethod]
+		public void Test6()
+		{
+			var bs = new BinarySearch();
+			var result = bs.FindRightMost(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 7, 7, 10 }, 10);
+			Assert.IsTrue(result == 9);
+		}
+
+		[TestMethod]
+		public void Test7()
+		{
+			var bs = new BinarySearch();
+			var result = bs.FindLeftMost(new List<int>() { 1, 2, 6, 6, 6, 6, 7, 7, 7, 10 }, 6);
+			Assert.IsTrue(result == 2);
+
+			result = bs.FindLeftMost(new List<int>() { 1, 2, 6, 6, 6, 6, 7, 7, 7, 10 }, 7);
+			Assert.IsTrue(result == 6);
+		}
+
+		[TestMethod]
+		public void Test8()
+		{
+			var bs = new BinarySearch();
+			var result = bs.FindRightMost(new List<int>() { 1, 2, 6, 6, 6, 6, 7, 7, 7, 10 }, 6);
+			Assert.IsTrue(result == 5);
+
+			result = bs.FindRightMost(new List<int>() { 1, 2, 6, 6, 6, 6, 7, 7, 7, 10 }, 7);
+			Assert.IsTrue(result == 8);
+		}
+	}
+}
---
+++ UnitTestProject/MaxesAndNumsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codesthenics;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class MaxesAndNumsTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			var mn = new MaxesAndNums();
+			var result = mn.Find(new List<int>() { 1, 2, 4, 4 }, new List<int>() { 3, 5 });
+			Assert.IsTrue(result[0] == 2);
+			Assert.IsTrue(result[1] == 4);
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			var mn = new MaxesAndNums();
+			var result = mn.Find(new List<int>() { 1, 2, 4, 4, 8, 4, 3, 6, 8, 4, 1, 5, 6, 3, 3, 7, 8 }, new List<int>() { 3, 5, 9, 1 });
+			Assert.IsTrue(result[0] == 6);
+			Assert.IsTrue(result[1] == 11);
+			Assert.IsTrue(result[2] == 17);
+			Assert.IsTrue(result[3] == 2);
+		}
+	}
+}
